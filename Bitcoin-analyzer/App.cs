4888f7e9f249c2532cfdd5281dd6ca39a11c2455
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2021-12-19 17:53:34 +0200

    User Interface
    Minimalistic user interface.

--- Bitcoin-analyzer/App.cs
+++ Bitcoin-analyzer/App.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
 
 namespace Bitcoin_analyzer
 {
-    class App
+    class App : GUI
     {
+        
         public static void Main (string[] args)
         {
             
+            GUI display = new GUI();
+            display.DisplayGUI();
+
         }
+
+        
     }
 }
---
+++ Bitcoin-analyzer/GUI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bitcoin_analyzer
+{
+    public class GUI
+    {
+        Form newForm = new Form();
+        DateTimePicker startDate = new DateTimePicker();
+        DateTimePicker endDate = new DateTimePicker();
+        Label startDateLabel = new Label();
+        Label endDateLabel = new Label();
+        Button analyzeButton = new Button();
+        TextBox resultsText = new TextBox();
+        Font smallFont = new Font("Arial", 10);
+
+        public void DisplayGUI()
+        {
+            
+            newForm.Name = "Uncle Scrooge's bitcoin analyzer";
+            newForm.Text = "Uncle Scrooge's bitcoin analyzer";
+            newForm.Size = new Size(380, 400);
+            newForm.StartPosition = FormStartPosition.CenterScreen;
+                  
+            startDate.Format = DateTimePickerFormat.Short;
+            startDate.Size = new Size(100, 20);
+            startDate.Location = new Point(100, 30);
+            newForm.Controls.Add(startDate);
+     
+            endDate.Format = DateTimePickerFormat.Short;
+            endDate.Size = new Size(100, 20);
+            endDate.Location = new Point(100, 70);
+            newForm.Controls.Add(endDate);
+
+            startDateLabel.Font = smallFont;
+            startDateLabel.Text = "Start date";      
+            startDateLabel.Size = new Size(100, 20);
+            startDateLabel.Location = new Point(30, 30);
+            newForm.Controls.Add(startDateLabel);
+
+            endDateLabel.Font = smallFont;
+            endDateLabel.Text = "End date";
+            endDateLabel.Size = new Size(100, 20);
+            endDateLabel.Location = new Point(30, 70);
+            newForm.Controls.Add(endDateLabel);
+
+            analyzeButton.Text = " Analyze!";
+            analyzeButton.Size = new Size(100, 60);
+            analyzeButton.Location = new Point(230, 30);
+            analyzeButton.Click += new EventHandler(ShowResults);
+            newForm.Controls.Add(analyzeButton);
+            
+            resultsText.Multiline = true;
+            resultsText.ReadOnly = true;
+            resultsText.Size = new Size(300, 200);
+            resultsText.Name = "Result Text";
+            //resultsText.Text = "results";
+            resultsText.Location = new Point(30, 120);
+            newForm.Controls.Add(resultsText);
+            
+            Application.Run(newForm);
+        }
+
+        private void ShowResults(object source, EventArgs e)
+        {
+            /*Analyzer newAnalysis = new Analyzer();
+            newAnalysis.Request(startDate.Value, endDate.Value);
+                       
+            resultsText.Text = "Longest downward trend: " + newAnalysis.numberOfDays + " days. \n";
+            resultsText.AppendText("Highest trading volume: " + newAnalysis.highestVolume[1] + "€. \n");
+
+            if (newAnalysis.buyAndSellDays != null)
+            {
+                resultsText.AppendText("Best day to buy: " + newAnalysis.buyAndSellDays[0].ToShortDateString() + "\n");
+                resultsText.AppendText("Best day to sell: " + newAnalysis.buyAndSellDays[1].ToShortDateString() + "\n");
+            }
+            else
+            {
+                resultsText.AppendText("No good days to buy or sell.");
+           }*/
+        }
+    }
+}
+       
+
